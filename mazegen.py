2f8370b62c1d946d749b4fe3c2549af6fe5953eb
dekkerglen <dekker.glen@gmail.com>
2020-10-24 20:52:31 -0400

    add braid maze implementation

--- mazegen.py
+++ mazegen.py
@@ -1,9 +1,10 @@
 import argparse
 import random
 import sys
+from util import Directions
 
 
-MAZE_CHARS = ['  ','██']
+MAZE_CHARS = ['  ','██','▒▒','░░']
 PERFECT_MAZE = 'perfect'
 BRAID_MAZE = 'braid'
 
@@ -22,6 +23,12 @@ def border(maze, width, height):
       if i == 0 or i == height - 1 or j == 0 or j == width - 1:
         maze[i][j] = 1
 
+def pillars(maze, width, height):
+  for x in range(width):
+    for y in range(height):
+      if x % 2 == 0 and y % 2 == 0:
+        maze[y][x] = 1
+
 def has_dead_ends(maze, width, height):
   for x in range(width):
     for y in range(height):
@@ -36,16 +43,40 @@ def has_dead_ends(maze, width, height):
           return True
   return False
 
+def add_access(maze, width, height, start):
+  queue = []
+  visited = set()
+
+  queue.append(start)
+  while len(queue) > 0:
+    x, y = queue.pop()
+    maze[y][x] = 2
+    visited.add((x, y))
+    for direction in Directions.LIST:
+      dx, dy = Directions.TO_VECTOR[direction]
+      x2, y2 = int(x + dx), int(y + dy)
+      if (x2, y2) not in visited and maze[y2][x2] == 0:
+        queue.append((x2, y2))
+
+def remove_access(maze, width, height):
+  for x in range(width):
+    for y in range(height):
+      if maze[y][x] == 2:
+        maze[y][x] = 0
+
+def maze_contains(maze, width, height, item):
+  for x in range(width):
+    for y in range(height):
+      if maze[y][x] == item:
+        return (x, y)
+
+  return False
+
 
 def braid_maze(width, height):
   maze = zeroes(width, height)
   border(maze, width, height)
-
-  for x in range(width):
-    for y in range(height):
-      if x % 2 == 0 and y % 2 == 0:
-        maze[y][x] = 1
-
+  pillars(maze, width, height)
 
   queue = []
   
@@ -64,6 +95,24 @@ def braid_maze(width, height):
     if has_dead_ends(maze, width, height):
       maze[y][x] = 0
 
+  add_access(maze, width, height, (1, 1))
+  # take down walls that can't be accessed
+  while maze_contains(maze, width, height, 0):    
+    removed_wall = False
+    for x in range(width):
+      for y in range(height):
+        if maze[y][x] == 1 and not removed_wall and not (y == 0 or y == height - 1 or x == 0 or x == width - 1):
+          if maze[y-1][x] == 0 and maze[y+1][x] == 2 or  maze[y-1][x] == 2 and maze[y+1][x] == 0:
+            maze[y][x] = 0
+            removed_wall = True
+          elif maze[y][x-1] == 0 and maze[y][x+1] == 2 or  maze[y][x-1] == 2 and maze[y][x+1] == 0:
+            maze[y][x] = 0
+            removed_wall = True
+            
+    remove_access(maze, width, height)
+    add_access(maze, width, height, (1, 1))
+
+  remove_access(maze, width, height)
   return maze
 
 def perfect_maze(width, height):
--- util.py
+++ util.py
@@ -28,7 +28,7 @@ class Directions:
     NORTH: (0, 1),
     SOUTH: (0, -1),
     EAST: (1, 0),
-    EAST: (-1, 0),
+    WEST: (-1, 0),
   }
                
 
